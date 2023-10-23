82dd0765c9a37c3b99c90c6015ea9c442db5ae88
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-10-23 18:02:28 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import hljs from 'highlight.js';
+	let code = '\n<pre>\n\t<code>{code}</code>\n</pre>';
+	$: codeFormated = hljs.highlight(code, { language: 'html' }).value;
+</script>
+
+<!-- Один блок с contenteditable='true' с прозрачным фоном, поверх другого блока -->
+<div class="position-relative">
+	<pre class="form-control position-absolute" style="min-height:20em">
+        {@html codeFormated}
+    </pre>
+
+	<pre
+		class="form-control position-absolute"
+		style="min-height:20em; background-color:transparent; color:transparent;caret-color: black"
+		contenteditable="true"
+		bind:innerText={code}
+	/>
+</div>
